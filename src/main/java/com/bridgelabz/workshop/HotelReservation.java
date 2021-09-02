ad1abc1288de2f885cbd5637c11a7994d53ef022
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-02 08:22:16 +0530

    Added: Project files

---
+++ src/main/java/com/bridgelabz/workshop/HotelReservation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.workshop;
+
+/**
+ * Hotel Reservation Project
+ *
+ */
+public class HotelReservation
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hotel Reservation!" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/workshop/HotelReservationTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.workshop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HotelReservation.
+ */
+public class HotelReservationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
